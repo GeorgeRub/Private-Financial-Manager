5ba52605b4d7c70e5ae393e0b101b65ced3c3ddb
Heorhii Rublov <rublev.georgij@gmail.com>
2025-01-15 15:36:59 -0400

    added side nav bar from material

--- front-angular/src/app/app.component.ts
+++ front-angular/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatDrawer, MatDrawerContainer} from '@angular/material/sidenav';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatDrawerContainer, MatDrawer, MatButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
