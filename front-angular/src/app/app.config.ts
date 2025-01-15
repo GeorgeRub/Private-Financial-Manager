dd40368ebfaafbfbdfdb01dbc783628510513464
Heorhii Rublov <rublev.georgij@gmail.com>
2025-01-15 13:28:38 -0400

    angular adding keycloak

---
+++ front-angular/src/app/app.config.ts
@@ -0,0 +1,44 @@
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
+
+import {routes} from './app.routes';
+import {
+  AutoRefreshTokenService,
+  createInterceptorCondition, INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG, IncludeBearerTokenCondition,
+  provideKeycloak,
+  UserActivityService,
+  withAutoRefreshToken
+} from 'keycloak-angular';
+
+export const provideKeycloakAngular = () =>
+  provideKeycloak({
+    config: {
+      url: 'http://local.keycloak.com',
+      realm: 'private-financial-manager',
+      clientId: 'pfm'
+    },
+    initOptions: {
+      onLoad: 'check-sso',
+      silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html'
+    },
+    features: [
+      withAutoRefreshToken({
+        onInactivityTimeout: 'logout',
+        sessionTimeout: 60000
+      })
+    ],
+    providers: [AutoRefreshTokenService, UserActivityService]
+  });
+
+const urlCondition = createInterceptorCondition<IncludeBearerTokenCondition>({
+  urlPattern: /^(http:\/\/localhost:8080)(\/.*)?$/i,
+  bearerPrefix: 'Bearer'
+});
+
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideKeycloakAngular(), {
+    provide: INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG,
+    useValue: [urlCondition] // <-- Note that multiple conditions might be added.
+  }, provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes)]
+};
