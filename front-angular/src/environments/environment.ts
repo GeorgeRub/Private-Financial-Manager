8144c7cbfb6e111d41c93146f8c5bb9b36a0114c
Heorhii Rublov <rublev.georgij@gmail.com>
2025-02-02 14:42:37 -0400

    change typescript version

---
+++ front-angular/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  back_http: "http://localhost:8082",
+  api_version: "/api/v1/",
+  production: false,
+  keycloak: {
+    authority: 'http://local.keycloak.com',
+    redirectUri: 'http://localhost:4200',
+    postLogoutRedirectUri: 'http://localhost:4200/logout',
+    realm: 'private-financial-manager',
+    clientId: 'pfm',
+  },
+  idleConfig: { idle: 10, timeout: 60, ping: 10 },
+}
